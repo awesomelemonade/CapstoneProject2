778568da12fa6d8d136150243928bc4c6fb35822
Anna Cai <anana10c@gmail.com>
2018-07-19 15:53:57 -0400

    created main

---
+++ main.py
@@ -0,0 +1,17 @@
+import databasing as db
+import Camera_to_Descriptor as camdes
+import clustering
+
+database = db.DescriptorDatabase('database.txt')
+
+def log_person(name, folder_path):
+    '''
+    Parameters:
+    name - string
+    folder_path - string
+        path to folder with images
+    '''
+    list_of_arr = camdes.file_to_descriptor(folder_path)
+    descriptors = [descriptor for arr in list_of_arr for descriptor in arr]
+    avg_des = get_avg_descriptor(descriptors)
+    database.put(name, avg_des)
