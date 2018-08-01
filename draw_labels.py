bf905b50e12a4e04dc8466093fee4f797ae82596
New Annabelle <lewa@bxscience.edu>
2018-08-01 14:31:43 -0400

    made face recognition into a package

--- draw_labels.py
+++
@@ -1,7 +0,0 @@
-def draw_labels(ax, fig, rect, text):
-    # Get the landmarks/parts for the face in box d.
-    # Draw the face landmarks on the screen.
-    rectangle = Rectangle((rect.left(), rect.top()), rect.width(), rect.height(), linewidth = 1, edgecolor = 'b', facecolor = 'none')
-    ax.add_patch(rectangle)
-    ax.text(rect.center().x, rect.center().y, text, ha = "center", va = "center", color="#FFFFFF")
-    
